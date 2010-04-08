168a7f33864d3c79b0681567f4b29303394e975b
Fabien.Arpinon <Fabien.Arpinon@7c5e934e-0b12-11de-9036-69e38a880166>
2010-04-08 22:34:52 +0000

    TAJ proc

--- DKSimVB/Procs/WeaponProc.vb
+++ DKSimVB/Procs/WeaponProc.vb
@@ -60,8 +60,15 @@ Public Class WeaponProc
 					if sim.MainStat.DualW then
 						If Rng4 > 0.5 Then
 							tmp = sim.MainHand.AvrgNonCrit(T)/2
+							me.CD = T+1
+							sim.TryOnMHHitProc
+							me.CD = 0
 						Else
+							
 							tmp = sim.offhand.AvrgNonCrit(T)/2
+							Me.CD = T+1
+							sim.TryOnOHHitProc
+							me.CD = 0
 						End If
 					Else
 						tmp = sim.MainHand.AvrgNonCrit(T)/2
